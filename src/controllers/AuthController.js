9b54c17349136d8e9e26b2ac6d8bd14aa708886c
Diego Salas <diesalasmart@gmail.com>
2022-06-04 23:33:17 -0500

    login route

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,51 +1,29 @@
-// const User = require('../models/UserModel')
 const { StatusCodes } = require("http-status-codes")
+const db = require("../db/models/index")
 const { BadRequestError, UnauthenticatedError } = require("../errors")
 
 const login = async (req, res) => {
-  // const { username, password } = req.body;
-  // if (!username || !password) {
-  //     throw new BadRequestError('Please provide an username and password');
-  // }
-  // const user = await User.findOne({username});
-  // if (!user) {
-  //     throw new UnauthenticatedError('User doesn\'t exist');
-  // }
-  // const correctPassword = await user.comparePassword(password);
-  // if (!correctPassword) {
-  //     throw new UnauthenticatedError('Username or password did not match');
-  // }
-  // const token = user.createJWT();
-  // const userResponse = {
-  //     _id: user._id,
-  //     name: user.name,
-  //     lastName: user.lastName,
-  //     username: user.username
-  // }
-  // res.status(StatusCodes.OK).json({user: userResponse, token});
-}
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new BadRequestError('Please provide an username and password')
+  }
+  const user = await db.Usuario.findOne({ where: { usuario: username }})
+  if (!user) {
+    throw new UnauthenticatedError(`No existe el usuario ${username}`)
+  }
+  const correctPassword = await user.comparePassword(password)
+  if (!correctPassword) {
+    throw new UnauthenticatedError('Credenciales inválidas')
+  }
 
-const register = async (req, res) => {
-  // const newUser = new User({
-  //     name: req.body.name,
-  //     lastName: req.body.lastName,
-  //     username: req.body.username,
-  //     password: req.body.password,
-  //     email: req.body.email,
-  //     birthday: req.body.birthday
-  // })
-  // const user = await newUser.save();
-  // const token = user.createJWT();
-  // const userResponse = {
-  //     _id: user._id,
-  //     name: user.name,
-  //     lastName: user.lastName,
-  //     username: user.username
-  // }
-  // res.status(StatusCodes.OK).json({user: userResponse, token});
+  const token = user.createJWT()
+  const userResponse = {
+    id: user.id,
+    nombre: user.getFullName()
+  }
+  res.status(StatusCodes.OK).json({ usuario: userResponse, token })
 }
 
 module.exports = {
-  login,
-  register
+  login
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const authRoutes = require("./AuthRoutes")
+const usuarioRoutes = require("./UsuarioRoutes")
 const productoRoutes = require("./ProductoRoutes")
 
 module.exports = {
   authRoutes,
+  usuarioRoutes,
   productoRoutes
 }
