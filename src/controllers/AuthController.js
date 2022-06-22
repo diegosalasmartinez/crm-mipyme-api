4935d1ffb6a6db8e892187e130d6c4211a28e5a4
Diego Salas <diesalasmart@gmail.com>
2022-06-21 19:49:12 -0500

    throw errors in auth validation

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,19 +1,19 @@
-const { StatusCodes } = require("http-status-codes")
 const db = require("../db/models/index")
-const { BadRequestError, UnauthenticatedError } = require("../errors")
+const { StatusCodes } = require("http-status-codes")
+const { BadRequestError } = require("../errors")
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
   if (!username || !password) {
-    throw new BadRequestError('Please provide an username and password')
+    throw new BadRequestError('Por favor, ingrese el usuario y contraseña')
   }
   const user = await db.Usuario.findOne({ where: { usuario: username }})
   if (!user) {
-    throw new UnauthenticatedError(`No existe el usuario ${username}`)
+    throw new BadRequestError(`No existe el usuario ${username}`)
   }
   const correctPassword = await user.comparePassword(password)
   if (!correctPassword) {
-    throw new UnauthenticatedError('Credenciales inválidas')
+    throw new BadRequestError('Credenciales inválidas')
   }
 
   const token = user.createJWT()
