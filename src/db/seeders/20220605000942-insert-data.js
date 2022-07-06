480ae2321534fad27d1f69c74e6346faf8159c54
Diego Salas <diesalasmart@gmail.com>
2022-07-05 19:31:38 -0500

    update seeders

--- src/db/seeders/20220605000942-insert-data.js
+++ src/db/seeders/20220605000942-insert-data.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid')
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Empresa', [{
+    const empresas = await queryInterface.bulkInsert('Empresa', [{
       id: uuidv4(),
       nombre: 'Tambo',
       direccion: 'Urb A Calle B',
@@ -11,9 +11,31 @@ module.exports = {
       activo: true,
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {})
+    }], { returning: true })
+    const usuarios = await queryInterface.bulkInsert('Usuario', [{
+      id: uuidv4(),
+      nombre: 'Diego',
+      apePaterno: 'Salas',
+      apeMaterno: 'Martinez',
+      usuario: 'diego',
+      password: process.env.TEST_USER_PASSWORD,
+      email: 'diesalasmart@gmail.com',
+      activo: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], { returning: true })
+    await queryInterface.bulkInsert('EmpresaUsuario', [{
+      id: uuidv4(),
+      usuarioId: usuarios[0].id,
+      empresaId: empresas[0].id,
+      activo: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }])
   },
   async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('EmpresaUsuario', null, {})
     await queryInterface.bulkDelete('Empresa', null, {})
+    await queryInterface.bulkDelete('Usuario', null, {})
   }
 }
