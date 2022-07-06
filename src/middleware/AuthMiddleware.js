a4773329fec3f5287d94a7aeb7d6de0563c89036
Diego Salas <diesalasmart@gmail.com>
2022-07-05 19:31:08 -0500

    delete console logs

--- src/middleware/AuthMiddleware.js
+++ src/middleware/AuthMiddleware.js
@@ -4,14 +4,12 @@ const { UnauthenticatedError } = require("../errors")
 
 const auth = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  console.log(authHeader);
   if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new UnauthenticatedError('No tienes acceso a esta funcionalidad')
   }
   const token = authHeader.split(' ')[1]
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
-    console.log(payload)
     const usuario = await db.Usuario.findOne({
       where: { id: payload.usuarioId },
       attributes: ['id', 'activo']
