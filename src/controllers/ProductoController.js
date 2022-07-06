61160d657524488648cd0846270dd5e5d7db9ff3
Diego Salas <diesalasmart@gmail.com>
2022-07-05 23:40:24 -0500

    modify where clause to include empresaId validation

--- src/controllers/ProductoController.js
+++ src/controllers/ProductoController.js
@@ -3,10 +3,13 @@ const { StatusCodes } = require("http-status-codes")
 
 const listarProductos = async (req, res) => {
   const { page = 0, rowsPerPage = 10 } = req.query
+  const empresaId = req.usuario.empresaId
+
   const productos = await db.Producto.findAll({
     offset: page * rowsPerPage,
     limit: rowsPerPage,
     where: {
+      empresaId,
       activo: true
     }
   })
