1fb21a15be0df668745d30b88311a5acd1a21b99
Diego Salas <Diego.Salas@promart.pe>
2022-10-18 00:14:44 -0500

    create activity and type models

---
+++ src/db/migrations/20221018044252-create-activities-table.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('activities', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      idDeal: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'deals',
+          key: 'id',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+        allowNull: true,
+      },
+      idType: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'activity_type',
+          key: 'id',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      startDate: Sequelize.DATE,
+      endDate: Sequelize.DATE,
+      notes: Sequelize.STRING,
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+  // eslint-disable-next-line no-unused-vars
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('activities');
+  }
+};
