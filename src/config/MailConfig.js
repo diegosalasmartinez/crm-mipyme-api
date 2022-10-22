7b204791ddc04d294343c80f83b1a0cc82efd71c
Diego Salas <Diego.Salas@promart.pe>
2022-10-22 17:43:59 -0500

    nodemailer config

---
+++ src/config/MailConfig.js
@@ -0,0 +1,18 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+});
+
+transporter.verify().then(() => {
+  console.log('Ready for send emails');
+});
+
+module.exports = { transporter };
