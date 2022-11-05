536960ffb4b916bd206d4f48ab934ad6db208ecc
Diego Salas <Diego.Salas@promart.pe>
2022-11-05 18:08:33 -0500

    update img tag

--- src/services/MailService.js
+++ src/services/MailService.js
@@ -9,20 +9,19 @@ class MailService {
       const position = htmlDecoded.indexOf('</body');
 
       for (const lead of campaign.leads) {
-        const imageTag = `<img src="${process.env.MAIL_HOST}/${campaign.id}/${lead.id}">`;
+        const imageTag = `<img src="${process.env.MAIL_HOST}/${campaign.id}/${lead.id}" alt="track_visit" width="1" height="1">`;
         const htmlFormatted = [
           htmlDecoded.slice(0, position),
           imageTag,
           htmlDecoded.slice(position),
         ].join('');
-        console.log(htmlFormatted)
 
-        // await transporter.sendMail({
-        //   from: `${company.name} <${company.email}>`,
-        //   to: lead.email,
-        //   subject: campaign.name,
-        //   html: htmlFormatted,
-        // });
+        await transporter.sendMail({
+          from: `${company.name} <${company.email}>`,
+          to: lead.email,
+          subject: campaign.name,
+          html: htmlFormatted,
+        });
       }
     } catch (e) {
       throw new BadRequestError(e.message);
