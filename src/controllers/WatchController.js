46202403cb33b18b45a54a9327542c3184a39e76
Diego Salas <Diego.Salas@promart.pe>
2022-11-05 19:44:49 -0500

    update logs

--- src/controllers/WatchController.js
+++ src/controllers/WatchController.js
@@ -5,9 +5,9 @@ const campaignService = new CampaignService();
 const verifyRead = async (req, res) => {
   const { idLead, idCampaign } = req.params;
   console.log(idLead, idCampaign)
-  // await campaignService.increaseScopeCampaign(idCampaign, idLead);
-  // res.status(StatusCodes.OK).sendBlankGif();
-  res.sendFile('../assets/mipyme.svg')
+  await campaignService.increaseScopeCampaign(idCampaign, idLead);
+  res.status(StatusCodes.OK).sendBlankGif();
+  // res.sendFile('../assets/mipyme.svg')
 };
 
 module.exports = {
