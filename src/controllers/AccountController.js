67af6ca862fcd1baab1758c6c3cbf63c93b1e5a3
Diego Salas <Diego.Salas@promart.pe>
2022-11-06 00:54:01 -0500

    update account controller

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,12 +1,15 @@
 const { StatusCodes } = require('http-status-codes');
 const RoleService = require('../services/RoleService');
 const roleService = new RoleService();
+const MarketingKPIService = require('../services/MarketingKPIService');
+const marketingKPIService = new MarketingKPIService();
 
-const getRoles = async (req, res) => {
+const getAccount = async (req, res) => {
   const roles = await roleService.getRoles();
-  res.status(StatusCodes.OK).json(roles);
+  const kpis = await marketingKPIService.getAll();
+  res.status(StatusCodes.OK).json({ roles, kpis });
 };
 
 module.exports = {
-  getRoles,
+  getAccount,
 };
